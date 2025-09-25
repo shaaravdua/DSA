4b6ca8f1ad30311dacbc6e478e7bcfc9158dd6e1
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2025-09-25 17:12:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Elements of Array/print-elements-of-array.cpp
@@ -0,0 +1,11 @@
+// User function template for C++
+class Solution {
+  public:
+    // Just print the space seperated array elements
+    void printArray(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            cout<< arr[i] << " " ;
+        }
+    }
+};
