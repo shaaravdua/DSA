bd7e87db501a63613bc2d0cc9449da5091f8abe8
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2025-09-26 19:10:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int findSum(int n) {
+        // code here
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+};
