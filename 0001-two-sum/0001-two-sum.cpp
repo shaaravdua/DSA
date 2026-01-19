ab86a2591877e96f885abf03b016355d115b4f6e
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-01-19 23:56:37 +0530

    Time: 0 ms (100%), Space: 14.7 MB (52.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,6 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             int need=target-nums[i];
-        
             if(mp.find(need)!=mp.end()){
                 return {mp[need],i};
             }
