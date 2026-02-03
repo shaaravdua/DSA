989e9dbde8f31ffa8bc4e30dc8cf426a1dcc6da9
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-02-03 23:38:13 +0530

    Time: 0 ms (100%), Space: 9 MB (20.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char &ch:s){
+            if(st.empty()||ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else if(ch==')'){
+                if(st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch==']'){
+                if(st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch=='}'){
+                if(st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
