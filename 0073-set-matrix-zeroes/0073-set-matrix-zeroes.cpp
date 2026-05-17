465cec454198bc46e17762cabb3a7e9bcee553e2
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-05-17 14:06:24 +0530

    Time: 0 ms (100%), Space: 20.7 MB (75.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,26 +4,20 @@ public:
         if (mat.empty() || mat[0].empty()) return;
         int n=mat.size();
         int m=mat[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
         int MARKER = -2000000000;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    for(int col=0;col<m;col++){
-                        if(mat[i][col]!=0){
-                            mat[i][col]=MARKER;
-                        }
-                    }
-                    for(int row=0;row<n;row++){
-                        if(mat[row][j]!=0){
-                            mat[row][j]=MARKER;
-                        }
-                    }
+                    row[i]=MARKER;
+                    col[j]=MARKER;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==MARKER){
+                if(row[i]==MARKER || col[j]==MARKER){
                     mat[i][j]=0;
                 }
             }
