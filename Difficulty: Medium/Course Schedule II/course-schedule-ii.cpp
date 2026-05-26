c26303851116b2380610a218a4912891e6f9ae3f
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-05-26 13:59:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule II/course-schedule-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> findOrder(int n, vector<vector<int>> &P) {
+        // code here
+        vector<vector<int>> G(n);                   // {prerequisite-course : [list of next courses]}
+        vector<int> ans, indegree(n);               // indegree[i] denotes number of prerequisite courses for ith course
+        for(auto& pre : P) 
+            G[pre[1]].push_back(pre[0]),            // forming adjacency list graph
+            indegree[pre[0]]++;                     
+        
+        queue<int> q;
+        for(int i = 0; i < n; i++)
+            if(indegree[i] == 0) q.push(i);         // we can start with courses having no prequisites
+        
+        while(size(q)) {
+            auto cur = q.front(); q.pop();
+            ans.push_back(cur);                     // cur has no remaining pre courses, so we can take it now
+            for(auto nextCourse : G[cur]) 
+                if(--indegree[nextCourse] == 0)     // if there's a next course having 0 prequisite remaining,
+                    q.push(nextCourse);             // then we can take it
+        }
+        if(size(ans) == n) return ans;              // ordering exists where all courses can be finished
+        return {}; 
+        
+    }
+};
