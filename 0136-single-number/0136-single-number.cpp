efbd7c74c7c13c589e97cbb29ce9f407aa1abfe0
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-06-17 16:54:06 +0530

    Time: 14 ms (10.11%), Space: 24.1 MB (7.49%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,11 +5,11 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        for(int i=0;i<nums.size();i++){
-            if(mp[nums[i]]==1){
-                return nums[i];
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
             }
         }
-        return -1;
+        return 0;
     }
 };
