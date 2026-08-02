8d303cf6dbf79db476b5d16ad191e21680ce3e63
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-08-02 20:01:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -1,13 +1,17 @@
 class Solution {
-public:
+  public:
     int missingNum(vector<int>& arr) {
-        int n = (int)arr.size() + 1;   // array length is n-1
-        long long xor_all = 0;
-        long long xor_arr = 0;
-
-        for (long long i = 1; i <= n; ++i) xor_all ^= i;
-        for (int v : arr) xor_arr ^= (long long)v;
-
-        return static_cast<int>(xor_all ^ xor_arr);
+        // code here
+        int cnt=1;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==cnt){
+                cnt++;
+            }
+            else{
+                break;
+            }
+        }
+        return cnt;
     }
 };
