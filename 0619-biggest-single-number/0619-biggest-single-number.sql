76ad337c2570bc97440c3ab2973438f5ed52e0e0
Shaarav Dua <142678135+shaaravdua@users.noreply.github.com>
2026-08-16 20:30:51 +0530

    Time: 418 ms (87.19%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select max(num) as num from myNumbers where num in (select if(count(num)<2,num,null)
-from myNumbers group by num );
+select max(num) as num from myNumbers where num in (select num
+from myNumbers group by num having count(*)=1 );
